1b9c9c77093852343033ccc0ed75974597378e70
jacobwhood <jacobwhood@utexas.edu>
2018-09-29 16:54:01 -0500

    Add client code to directory

---
+++ client/src/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './Components/Editor.jsx';
+import io from "socket.io-client";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: ''
+    };
+
+    this.socket = io.connect();
+
+    this.socket.on('connect', () => {
+      console.log('connection made client side');
+    });
+
+    this.socket.on('newClientConnection', (code) => {
+      this.setState({ code: code });
+    });
+
+    this.socket.on('serverUpdateCode', (newCode) => {
+      console.log('im changing state: ', newCode);
+      this.setState({ code: newCode });
+    });
+
+    this.onCodeUpdate = this.onCodeUpdate.bind(this);
+  }
+
+  onCodeUpdate(newCode, e) {
+    console.log('newCode: ', newCode);
+    this.setState({ code: newCode }, () => {
+      this.socket.emit('clientUpdateCode', newCode);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='editor-container'>
+          <Editor
+            onCodeUpdate={this.onCodeUpdate}
+            code={this.state.code}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
