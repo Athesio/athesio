d19ee74506fbfb53bb509f8841ca487ae9c308b0
jacobwhood <jacobwhood@utexas.edu>
2018-09-30 17:37:38 -0500

    Minor style changes

--- client/src/Components/Firepad-ace.jsx
+++ client/src/Components/Firepad-ace.jsx
@@ -14,12 +14,13 @@ class FirepadAce extends Component {
     };
 
     window.firebase.initializeApp(config);
-    //// Get Firebase Database reference.
     let firepadRef = firebase.database().ref();
-    //// Create ACE
+    
+    // create Ace editor and config
     let editor = ace.edit("firepad-container");
     editor.setTheme("ace/theme/tomorrow_night");
     editor.$blockScrolling = 1;
+    
     let session = editor.getSession();
     session.setUseWrapMode(true);
     session.setUseWorker(false);
@@ -27,7 +28,7 @@ class FirepadAce extends Component {
     session.setMode("ace/mode/javascript");
 
     let firepad = window.Firepad.fromACE(firepadRef, editor, {
-      defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}'
+      defaultText: 'howdy world'
     });
   }
 
--- client/src/Components/Firepad.jsx
+++ client/src/Components/Firepad.jsx
@@ -15,15 +15,13 @@ class Firepad extends Component {
     };
 
     window.firebase.initializeApp(config);
+
     let firepadRef = firebase.database().ref();
+    
     let codeMirror = window.CodeMirror(document.getElementById('firepad-container'), 
       { lineNumbers: true, mode: {name: 'javascript', json: true }, theme: 'darcula', tabSize: 2 });
+
     let firepad = window.Firepad.fromCodeMirror(firepadRef, codeMirror);
-    // firepad.on('ready', function () {
-    //   if (firepad.isHistoryEmpty()) {
-    //     firepad.te('<span style="font-size: 24px;">Rich-text editing with <span style="color: red">Firepad!</span></span><br/><br/>Collaborative-editing made easy.\n');
-    //   }
-    // });
   }
 
   render() {
