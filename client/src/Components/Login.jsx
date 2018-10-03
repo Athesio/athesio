eee854a5fcfd10bc3f76f363770673b169ade323
jacobwhood <jacobwhood@utexas.edu>
2018-10-03 17:12:08 -0500

    Add login and logout components

---
+++ client/src/Components/Login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Login = (props) => {
+  <div>
+    <h2>
+      Login shit
+    </h2>
+  </div>
+};
+
+export default Login;
---
+++ client/src/Components/Logout.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Logout = (props) => {
+  <div>
+    <h2>
+      Logout shit
+    </h2>
+  </div>
+};
+
+export default Logout;
