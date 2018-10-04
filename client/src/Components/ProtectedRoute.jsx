7101145e3b9d17dfc72e30bb09d8a7d5883f367e
jacobwhood <jacobwhood@utexas.edu>
2018-10-04 17:54:10 -0500

    Implement authentication in React Router

--- client/src/Components/ProtectedRoute.jsx
+++ client/src/Components/ProtectedRoute.jsx
@@ -2,21 +2,44 @@ import React, { Component } from 'react';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 import axios from 'axios';
 
-const ProtectedRoute = ({ component: Component }) => {
-  return (
-    <Route
-      render={() =>
-        false ? (
-          <Component />
-        ) : (
-            <Redirect to='/login' />
-          )
-      }
-    />);
-  // // axios.get('/api/authstate')
-  // // .then(authState => {
-    
-  // });
-};
+class ProtectedRoute extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authstate: false
+    }
+    //let authstate = this.getAuthState();
+  }
+
+  async getAuthState() {
+    return await axios.get('/api/authstate')
+      .then(authState => {
+        return authState.data
+      });
+  };
+
+  componentDidMount() {
+    this.setState({ authstate: this.getAuthState() });
+  }
+
+  render() {
+    return (
+      <Route
+        render={() =>
+          this.state.authstate ? (
+            <Component />
+          ) : (
+              <Redirect to='/login' />
+            )
+        }
+      />);
+  }
+}
+
+// const ProtectedRoute = async ({ component: Component }) => {
+//   let authstate = await getAuthState();
+//   console.log('auth state: ', authstate);
+  
+// };
 
 export default withRouter(ProtectedRoute);
