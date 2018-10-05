8262cff1c24096cd84c50f99d3aa6539eb7c0f59
jacobwhood <jacobwhood@utexas.edu>
2018-10-05 14:55:47 -0500

    Add console logging to ProtectRoute component

--- client/src/Components/ProtectedRoute.jsx
+++ client/src/Components/ProtectedRoute.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const ProtectedRoute = (props) => (
-  <Route
-    render={() => props.authStatus ?
-      (<props.component authStatus={props.authStatus}/>) : (<Redirect to='/login' />)
-    }
-  />
-);
+const ProtectedRoute = (props) => {
+  console.log('im in protected route mutha fuckaaa');
+  return (
+    <Route
+      render={() => props.authStatus ?
+        (<props.component authStatus={props.authStatus} />) : (<Redirect to='/login' />)
+      }
+    />
+  );
+};
 
 export default ProtectedRoute;
