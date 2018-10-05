db82ffa3ee57b32c8013027db8b3d96b26801cf6
jacobwhood <jacobwhood@utexas.edu>
2018-10-05 12:17:57 -0500

    Added authentication validation step

--- client/src/Components/LandingPage.jsx
+++ client/src/Components/LandingPage.jsx
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute.jsx';
+import SelectRoom from './SelectRoom.jsx';
+import axios from 'axios';
 
-const LandingPage = (props) => (
-  <div>
-    <h2> Welcome to the best collaborative coding environment EVER </h2>
-    <Link to='/login'>Get started</Link>
-  </div>
-);
+class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      authStatus: false
+    }
+  }
+
+  async getAuthStatus() {
+    console.log('calling getAuthStatus')
+    return await axios.get('/api/authstatus')
+      .then(async authStatus => {
+        this.setState({
+          authStatus: authStatus.data,
+          loading: false
+        })
+      });
+  };
+
+  componentDidMount() {
+    this.getAuthStatus();
+  }
+
+  render() {
+    console.log('state in landing page: ', this.state);
+    if (this.state.loading) {
+      return (
+        <div>Loading...</div>
+      );
+    } else if (!this.state.loading && this.state.authStatus) {
+      return (<ProtectedRoute component={SelectRoom} authStatus={this.state.authStatus} />);
+    } else {
+      return (
+        <div>
+          <h2> Welcome to the best collaborative coding environment EVER </h2>
+          <Link to='/login'>Get started</Link>
+        </div>
+      );
+    }
+  }
+
+}
 
 export default LandingPage;
