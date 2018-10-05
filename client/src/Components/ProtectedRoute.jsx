cd59b3e7a755406b1ba08c482082fae2f3bbfc59
jacobwhood <jacobwhood@utexas.edu>
2018-10-05 12:18:15 -0500

    Refactor to functional and consume auth status from props

--- client/src/Components/ProtectedRoute.jsx
+++ client/src/Components/ProtectedRoute.jsx
@@ -1,40 +1,12 @@
 import React, { Component } from 'react';
-import { Route, Redirect, withRouter } from 'react-router-dom';
-import axios from 'axios';
+import { Route, Redirect } from 'react-router-dom';
 
-class ProtectedRoute extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authstate: null
+const ProtectedRoute = (props) => (
+  <Route
+    render={() => props.authStatus ?
+      (<Component />) : (<Redirect to='/login' />)
     }
-  }
+  />
+);
 
-  async getAuthState() {
-    return await axios.get('/api/authstate')
-      .then(authState => {
-        console.log('front end authentication state: ', authState.data);
-        return authState.data;
-      });
-  };
-  componentDidMount() {
-    this.setState({
-      authstate: this.getAuthState()
-    });
-  }
-
-  render() {
-    return (
-      <Route
-        render={() =>
-          this.state.authstate ? (
-            <Component />
-          ) : (
-              <Redirect to='/login' />
-            )
-        }
-      />);
-  }
-}
-
-export default withRouter(ProtectedRoute);
+export default ProtectedRoute;
