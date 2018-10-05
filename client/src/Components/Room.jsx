9d0c94dea2079c1e5a1346ebb524241d7add33ff
jacobwhood <jacobwhood@utexas.edu>
2018-10-05 15:44:28 -0500

    Use correct authentication check in Room component

--- client/src/Components/Room.jsx
+++ client/src/Components/Room.jsx
@@ -61,80 +61,75 @@ class Room extends React.Component {
 
   render() {
     console.log('room render : ', this.props.location);
-    if (this.state.loading) {
-      return (<div>Loading...</div>);
-    } else {
-      if (this.props.location.state.authStatus) {
-        return (
-          <div className="wrapper">
-            {/* USER NAVIGATION BAR */}
-            <nav id="userNav" className="sidenav">
-              {/* <UserNav close={this.closeNav} user={this.props.user}/> */}
-            </nav>
-            <span className="userSpan" onClick={this.openNav}>&#9776;</span>
+    if (localStorage.getItem('authenticated')) {
+      return (
+        <div className="wrapper">
+          {/* USER NAVIGATION BAR */}
+          <nav id="userNav" className="sidenav">
+            {/* <UserNav close={this.closeNav} user={this.props.user}/> */}
+          </nav>
+          <span className="userSpan" onClick={this.openNav}>&#9776;</span>
 
-            {/* MIDDLE SECTION OF DASHBOARD */}
-            <div id="Editor" className="container" >
+          {/* MIDDLE SECTION OF DASHBOARD */}
+          <div id="Editor" className="container" >
 
-              {/* NAME OF THE APPLICATION */}
-              <div className="row" >
-                <div className="col-md-6" >
-                  <h2 style={{ fontSize: '60px' }} >ATHESIO</h2>
-                </div>
-                <div className="col-md-6">
-                  <h2>Share room: {this.state.roomId}</h2>
-                </div>
+            {/* NAME OF THE APPLICATION */}
+            <div className="row" >
+              <div className="col-md-6" >
+                <h2 style={{ fontSize: '60px' }} >ATHESIO</h2>
               </div>
-
-              {/* SHOWS USERS IN THE SESSION */}
-              <div className="row">
-                <div className="col-xs-12 col-md-12" id="userDiv" >
-                  {otherUsers.map((user, i) => {
-                    return (
-                      <a key={i} style={{ color: '#f1f1f1', padding: '10px', fontSize: '12px', float: 'right' }} className="joinedUsers" >
-                        {/* <img className="img-circle text-right" id="userImg" src={user.img} /> {user.username} */}
-                      </a>
-                    )
-                  })
-                  }
-                </div>
+              <div className="col-md-6">
+                <h2>Share room: {this.state.roomId}</h2>
               </div>
-
-              {/* HOLDS BOTH THE FIREPAD AND THE IFRAME */}
-              <div className="row" >
-                <div className="col-xs-12s col-sm-12 col-md-12" >
-                  <EditorHolder roomId={this.state.roomId} />
-                </div>
-              </div>
-              <div className="row" >
-                <div className="col-md-11 col-lg-11" >
-                  {/* <button type="button" >Save</button>  */}
-                </div>
-                <div className="col-md-1 col-lg-1" >
-                  <button type="button"  >Run</button>
-                </div>
-              </div>
-
-              <span className="githubSpan" name="Github" onClick={this.openRightNav}>GitHub</span>
-              <span className="chatSpan" name="Chat" onClick={this.openRightNav}>Slack</span>
-              <span className="chatSpan" name="Testing" onClick={this.openRightNav} style={{ top: '100px', right: '15px' }} >Testing</span>
-              <span className="chatSpan" name="Tasks" onClick={this.openRightNav} style={{ top: '140px', right: '15px' }} >Tasks</span>
             </div>
 
-            {/* RIGHT NAVIGATION BAR */}
-            <nav id="rightNav" className="rightNav">
-              {this.state.clickedTab === 'github' ? <GithubNav close={this.closeRightNav} /> : ('')}
+            {/* SHOWS USERS IN THE SESSION */}
+            <div className="row">
+              <div className="col-xs-12 col-md-12" id="userDiv" >
+                {otherUsers.map((user, i) => {
+                  return (
+                    <a key={i} style={{ color: '#f1f1f1', padding: '10px', fontSize: '12px', float: 'right' }} className="joinedUsers" >
+                      {/* <img className="img-circle text-right" id="userImg" src={user.img} /> {user.username} */}
+                    </a>
+                  )
+                })
+                }
+              </div>
+            </div>
 
-              {this.state.clickedTab === 'chat' ? <ChatNav close={this.closeRightNav} /> : ('')}
-            </nav>
+            {/* HOLDS BOTH THE FIREPAD AND THE IFRAME */}
+            <div className="row" >
+              <div className="col-xs-12s col-sm-12 col-md-12" >
+                <EditorHolder roomId={this.state.roomId} />
+              </div>
+            </div>
+            <div className="row" >
+              <div className="col-md-11 col-lg-11" >
+                {/* <button type="button" >Save</button>  */}
+              </div>
+              <div className="col-md-1 col-lg-1" >
+                <button type="button"  >Run</button>
+              </div>
+            </div>
+
+            <span className="githubSpan" name="Github" onClick={this.openRightNav}>GitHub</span>
+            <span className="chatSpan" name="Chat" onClick={this.openRightNav}>Slack</span>
+            <span className="chatSpan" name="Testing" onClick={this.openRightNav} style={{ top: '100px', right: '15px' }} >Testing</span>
+            <span className="chatSpan" name="Tasks" onClick={this.openRightNav} style={{ top: '140px', right: '15px' }} >Tasks</span>
           </div>
-        )
-      } else {
-        console.log('im redirecting from Room to Login');
-        return (<Redirect to='/login' />)
-      }
+
+          {/* RIGHT NAVIGATION BAR */}
+          <nav id="rightNav" className="rightNav">
+            {this.state.clickedTab === 'github' ? <GithubNav close={this.closeRightNav} /> : ('')}
+
+            {this.state.clickedTab === 'chat' ? <ChatNav close={this.closeRightNav} /> : ('')}
+          </nav>
+        </div>
+      )
+    } else {
+      console.log('im redirecting from Room to Login');
+      return (<Redirect to='/login' />)
     }
-    
   }
 }
 
