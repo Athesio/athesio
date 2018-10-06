028b8f09cdfc5116512d4e67e1e386cd942dcc1e
Sieh Johnson <johnson_sieh@yahoo.com>
2018-10-06 15:27:40 -0500

    Re-organize UI/UX of Room component

--- client/src/Components/EditorHolder.jsx
+++ client/src/Components/EditorHolder.jsx
@@ -3,7 +3,7 @@ import Firepad from './Firepad.jsx';
 
 const EditorHolder = (props) => {
   return (
-    <div id="EditorBox" className="container" >
+    <div id="EditorBox" className="container" style={{ paddingRight: '0px' }} >
       {/* LEFT SIDE FIREPAD */}
       <div className="row">
         <div className="col-xs-12  col-sm-12 col-md-12 col-lg-6" id="Ace" >
@@ -15,6 +15,14 @@ const EditorHolder = (props) => {
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6" id='res' >
           <iframe className="resultBox"  >
           </iframe>
+        </div>
+      </div>
+      <div className="row" >
+        <div className="col-md-11 col-lg-11" >
+          {/* <button type="button" >Save</button>  */}
+        </div>
+        <div className="col-md-1 col-lg-1" >
+          <button type="button">Run</button>
         </div>
       </div>
     </div>
