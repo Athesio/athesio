39b15a6454d684c5098f56c64d109ca4f82fe731
Sieh Johnson <johnson_sieh@yahoo.com>
2018-10-05 23:32:10 -0500

    Integrate various css elements to diversify Landing page

--- client/src/Components/LandingPage.jsx
+++ client/src/Components/LandingPage.jsx
@@ -36,16 +36,38 @@ class LandingPage extends Component {
       return (<ProtectedRoute component={SelectRoom} />);
     } else {
       return (
-        <div className="container">
+        <div className="container" id="Landing">
+        <div className="row" id="titleBox" >
+          <div className="col-xs-12 col-lg-12 text-center" >
+            <a className="text-pop-up-top" style={{ fontSize: "250px" }} id="title">ATHESIO</a>
+          </div>
+          <div className="text-center" >
+            <a  style={{ color: '#ffffff'}}>____________________________________________________________________________</a>
+          </div>
+         </div>
           <div className="row" >
-            <div className="col-xs-10 col-lg-10" >
-              Athesio
-            </div>
-            <div className="col-xs-2 col-lg-2" >
-              <Link to='/login'>Get started</Link>
+            <div className="col-xs-12 col-lg-12 text-center" >
+              <h2> Welcome to the best collaborative coding environment  <em> EVER. </em> </h2>
             </div>
           </div>
-          <h2> Welcome to the best collaborative coding environment EVER </h2>
+          <div className="text-center">
+            <Link to='/login'>Get started</Link>
+          </div>
+
+          <div className="row" id="LandingTech" >
+            <div className="col-xs-6 col-md-6 col-lg-3" >
+              <a>Editing</a>
+            </div>
+            <div className="col-xs-6 col-md-6 col-lg-3" >
+              <a>GitHub Integration</a>
+            </div>
+            <div className="col-xs-6 col-md-6 col-lg-3" >
+              <a>Slack</a>
+            </div>
+            <div className="col-xs-6 col-md-6 col-lg-3" >
+              <a>Video Chatting</a>
+            </div>
+          </div>
         </div>
       );
     }
