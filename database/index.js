d2de7f489727f31a92a51e20639a72c195b96559
jacobwhood <jacobwhood@utexas.edu>
2018-10-07 12:35:03 -0500

    Create Schema and connect to RDS MySQL instance

--- database/index.js
+++ database/index.js
@@ -1,1 +1,20 @@
 // DATABASE
+const mysql = require('mysql');
+require('dotenv').config();
+let db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+module.exports.saveRoomInfo = (roomInfo, cb) => {
+  db.query('select * from users', (err, results) => {
+    if (err) {
+      console.log('error: ', err);
+    } else {
+      console.log('result : ', results);
+    }
+  });
+};
+
