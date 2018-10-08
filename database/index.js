e59fdd5a951993bdbe150dddf974628003e8a6d9
jacobwhood <jacobwhood@utexas.edu>
2018-10-08 08:50:36 -0500

    Add beginning database communication layer

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 // DATABASE
 const mysql = require('mysql');
-require('dotenv').config();
+
 let db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,12 +8,29 @@ let db = mysql.createConnection({
   database: process.env.DB_NAME
 });
 
-module.exports.saveRoomInfo = (roomInfo, cb) => {
+module.exports.saveUser = (user, cb) => {
+  console.log('user: ', user);
+  // db.query('insert into users (github_id, github_username)')
+};
+
+module.exports.saveRoomInfoForUser = (roomInfo, cb) => {
+  console.log('in saveRoomInfoForUser roomInfo: ', roomInfo);
+
+  // db.query('insert into * from users', (err, results) => {
+  //   if (err) {
+  //     console.log('error: ', err);
+  //   } else {
+  //     console.log('result : ', results);
+  //   }
+  // });
+};
+
+module.exports.getPreviousRoomsForUser = (user, cb) => {
   db.query('select * from users', (err, results) => {
     if (err) {
-      console.log('error: ', err);
+      console.log('error retrieving room info for user: ', err);
     } else {
-      console.log('result : ', results);
+      console.log('results retrieving previous rooms for user: ', results);
     }
   });
 };
