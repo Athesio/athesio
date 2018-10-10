cf66b42002fee51348d843ca0e6477e742c73e7a
jacobwhood <jacobwhood@utexas.edu>
2018-10-10 17:04:16 -0500

    Style chatbox

--- client/src/Components/Message.jsx
+++ client/src/Components/Message.jsx
@@ -2,22 +2,28 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 const Message = (props) => {
-  console.log('current message: ', props);
   return (
-    <div className='message-container' style={{ overflow: 'scroll' }}>
+    <div className='message-container'>
       {props.message.user.login === props.user.login ?
-        (<div className='user-message'>
-          <p>{props.message.text}</p>
-          <p>{moment(props.message.createTime).fromNow()}</p>
-          <p>{props.message.user.login}</p>
-        </div>) :
-        (<div className='other-user-message'>
-          <p>{props.message.text}</p>
-          <p>{moment(props.message.createTime).fromNow()}</p>
-          <p>{props.message.user.login}</p>
-        </div>)
+        ( <div className='user-message'>
+            <div className='message-text-div'>{props.message.text}</div>
+            <div className='timestamp-user-text-container text-right'>
+              <div className='name-div'>{props.message.user.login}</div>
+              <div className='timestamp-div'>{moment(props.message.createTime).fromNow()}</div>
+            </div>
+          </div>
+        ) :
+        (
+          <div className='other-user-message'>
+              <div className='message-text-div'>{props.message.text}</div>
+              <div className='timestamp-user-text-container text-left'>
+                <div className='name-div'>{props.message.user.login}</div>
+                <div className='timestamp-div'>{moment(props.message.createTime).fromNow()}</div>
+              </div>
+            </div>
+        )
       }
-    </div> 
+    </div>
   );
 };
 
