572b8d5fb0a84b0ec447acb11ce447fa701bed29
Sieh Johnson <johnson_sieh@yahoo.com>
2018-10-18 15:53:41 -0500

    Try to add content

--- client/src/Components/SelectRoom.jsx
+++ client/src/Components/SelectRoom.jsx
@@ -20,7 +20,7 @@ class SelectRoom extends Component {
       showDataGithubMode: false,
       showDataReplMode: false,
       modes: {
-        GithubMode: ['Github Mode' ,'Open a repo or gist from Github to open and collaboratively edit files.'],
+        GithubMode: ['Github Mode' ,'Open a repo from Github to open and collaboratively edit files.'],
         ReplMode: ['REPL.mode','Perfect for interviews and collaborative code writing/testing.']
       }
     }
@@ -34,6 +34,7 @@ class SelectRoom extends Component {
     this.addHover = this.addHover.bind(this);
     this.removeHover = this.removeHover.bind(this);
     this.showModeContent = this.showModeContent.bind(this);
+    this.changeDiv = this.changeDiv.bind(this);
   }
 
   createRoomId(cb) {
@@ -150,7 +151,6 @@ class SelectRoom extends Component {
     target.childNodes.length > 0 ? target.removeChild(target.childNodes[0]) : null;
     target.style.width = '50%';
     e.target.id === 'GithubMode' ? document.getElementById('ReplMode').style.width='50%' : document.getElementById('GithubMode').style.width='50%';
-    console.log(e.target.id);
     e.target.id === 'GithubMode' ? this.setState({ showDataGithubMode: false }) : this.setState({ showDataReplMode: false });    
   }
 
@@ -161,11 +161,18 @@ class SelectRoom extends Component {
     e.target.id.includes('G') ? GithubMode.removeChild(GithubMode.childNodes[0]) : ReplMode.removeChild( ReplMode.childNodes[0]); 
     e.target.id.includes('G') ? GithubMode.style.width='90%' :  ReplMode.style.width='90%';
     e.target.id.includes('G') ? ReplMode.style.width='10%' : GithubMode.style.width='10%';
+    console.log(e.target.id.includes('G'));
     e.target.id.includes('G') ? this.setState({ showDataGithubMode: true }) : this.setState({ showDataReplMode: true });
-
-   ;
+    console.log(this.state.showDataGithubMode)
   } 
 
+  changeDiv(e) {
+    console.log(e.target.id.includes('G'));
+    console.log(this.state.showDataGithubMode)
+    e.target.id.includes('G') ? this.setState({ showDataGithubMode: false }) : this.setState({ showDataReplMode: false });
+    e.target.id.includes('G') ? null : document.getElementById('ReplModeDetails').style.width='50%';
+    e.target.id.includes('G') ? document.getElementById('GithubMode').style.width='50%' : null;
+  }
 
   render() {
     if (this.state.loading) {
@@ -177,27 +184,84 @@ class SelectRoom extends Component {
       if (localStorage.getItem('authenticated') === 'true') {
         return (
           <div id="Morpheus"  >
-            <div id="GithubMode" onMouseEnter={this.addHover} onMouseLeave={this.removeHover} onClick={this.showModeContent}>
+            
             {
               this.state.showDataGithubMode === true ? 
-              <div className="dropdown"  >
+              <div id="GithubModeDetails" onMouseLeave={this.changeDiv} >
+                {/* <div className="dropdown"  >
                 <Button className="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-addHovered="false" >
                   Repos
                 </Button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton" >
+                <div className="dropdown-menu"  >
                   {
                     this.state.repos.map((repo, i) => {
                       return <a className="dropdown-item" key={i} onClick={()=>{ this.setState({ selectedRepo: repo.name }, () => { this.createNewRoom() })}} >{repo.name}</a>
                     })
                   }
                 </div>
-              </div> : null
+                 */}
+                <div className="container-fluid" id="SelectRoomBox" >
+                <div className="row" id="formBox" >
+                  <div className="col-md-1"   ></div>
+                  <div className="col-md-10"  >
+                    <form role="form" >
+                      <div className="form-group" style={{ marginLeft: '10px', marginRight: '10px' }} >
+                        <a className="text-center" >Join a Room </a><br />
+                        <div className="input-group">
+                          <input type="text" className="form-control" placeholder="Room Key" value={this.state.value} onChange={(e) => { this.setState({ value: e.target.value }) }}></input>
+
+                          <span className="input-group-btn">
+                            <Button
+                              className="btn"
+                              type="button"
+                              onClick={this.joinRoomIfValid}
+                            >JOIN</Button>
+                          </span>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="col-md-1" ></div>
+                </div>
+                {
+                  this.state.repos.length > 0 ?
+                    <div className="row" >
+                      <div className="col-md-1" ></div>
+                      <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10" >
+                        <div className="text-center" id="sessionTable" >
+                          <a > PREVIOUS SESSIONS</a>
+                        </div>
+                        <div className="table-responsive" >
+                          <table id="session" className="table col-xs-12 col-sm-12 col-md-12 col-lg-12" >
+                            <thead  >
+                              <tr>
+                                <th align="left" className="text-center" >Title</th>
+                                <th className="text-center" ></th>
+                                <th align="right" className="text-center" >Last Modified</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {
+                                this.state.repos.map((repo, i) => <Session info={repo} key={i} handleClick={()=>{ this.setState({ selectedRepo: repo.name }, () => { this.createNewRoom() })}}/>)
+                              }
+                            </tbody>
+
+                          </table>
+                        </div>
+                      </div>
+                      <div className="col-md-1" ></div>
+                    </div>
+                    :
+                  null
+                }
+              </div> 
+              </div>
+              : <div id="GithubMode" onMouseEnter={this.addHover} onMouseLeave={this.removeHover} onClick={this.showModeContent}></div>
             }
-            </div>
-            <div id="ReplMode" onMouseEnter={this.addHover} onMouseLeave={this.removeHover} onClick={this.showModeContent} >
-            {this.state.showDataReplMode ? 
             
-              <div className="container-fluid" id="SelectRoomBox" >
+            {this.state.showDataReplMode === true ? 
+              <div id="ReplModeDetails" onMouseLeave={this.changeDiv} >
+                <div className="container-fluid" id="SelectRoomBox" >
                 <div className="row" id="formBox" >
                   <div className="col-md-1"   ></div>
                   <div className="col-md-10"  >
@@ -257,9 +321,10 @@ class SelectRoom extends Component {
                   null
                 }
               </div>
-            : null
+              </div>
+            : <div id="ReplMode" onMouseEnter={this.addHover} onMouseLeave={this.removeHover} onClick={this.showModeContent} ></div>
             } 
-            </div>
+            
           </div>
         )
       } else {
